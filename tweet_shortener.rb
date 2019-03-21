4c8d26aa0ba1498711c7f191bb46c718fbcdc464
Matthew Bartl <mattbartl@gmail.com>
2019-03-21 15:27:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,16 +1,3 @@
-dictionary = {
-  'hello': 'hi',
-  'to': '2',
-  'two': '2',
-  'too': '2',
-  'for': '4',
-  'four': '4',
-  'be': 'b',
-  'you': 'u',
-  'at': '@',
-  'and': '&'
-}
-
 def word_substituter(string)
   dictionary = {
     'hello': 'hi',
