8223e83699fb6f887399d154888a17be242d7f1e
Matthew Bartl <mattbartl@gmail.com>
2019-03-21 15:20:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,7 +12,18 @@ dictionary = {
 }
 
 def word_substituter(string)
-  dictionary
+  dictionary = {
+    'hello': 'hi',
+    'to': '2',
+    'two': '2',
+    'too': '2',
+    'for': '4',
+    'four': '4',
+    'be': 'b',
+    'you': 'u',
+    'at': '@',
+    'and': '&'
+  }
   tweet_array = string.split(/ /)
   tweet_array.each do
     i = 0
