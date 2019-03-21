95fccc034ea72e1be886560751851f4a791d5b3f
Matthew Bartl <mattbartl@gmail.com>
2019-03-21 15:28:47 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,4 +27,32 @@ def word_substituter(string)
   return tweet_array.join(' ')
 end
         
-      
+def bulk_tweet_shortener(array)
+    dictionary = {
+    'hello': 'hi',
+    'to': '2',
+    'two': '2',
+    'too': '2',
+    'for': '4',
+    'four': '4',
+    'be': 'b',
+    'you': 'u',
+    'at': '@',
+    'and': '&'
+  }
+  
+  tweet_array = string.split(/ /)
+  tweet_array.each do
+    i = 0
+    while i < tweet_array.length do
+      dictionary.each do |word, substitute|
+        word_holder = word.to_s
+        if tweet_array[i] == word_holder
+          tweet_array[i] = substitute
+        end
+      end
+      i += 1
+    end
+  end
+  return tweet_array.join(' ')
+end
